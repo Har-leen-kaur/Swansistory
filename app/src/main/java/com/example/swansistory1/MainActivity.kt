940148bf7712632fff0197c296e2ccbaa4046b7f
haka03 <haka03@dfki.de>
2022-03-31 12:53:01 +0100

    separate ThumbnailActivity from MainActivity

--- app/src/main/java/com/example/swansistory1/MainActivity.kt
+++ app/src/main/java/com/example/swansistory1/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.swansistory1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,36 +15,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val thumbnailList = populateList()
-        val recyclerView = findViewById<View>(R.id.myRecyclerView) as RecyclerView
-        val layoutManager = LinearLayoutManager(this)
 
-        recyclerView.layoutManager = layoutManager
-        val mAdapter = ThumbnailAdapter(thumbnailList)
-        recyclerView.adapter = mAdapter
-
-    }
-
-    private fun populateList(): ArrayList<Thumbnails_poi> {
-        val list = ArrayList<Thumbnails_poi>()
-
-        // Nasty handcoded array of images
-        val myImageList = arrayOf(R.drawable.swansea_castle, R.drawable.mumbles_and_swansea_bay, R.drawable.clyne_gardens,
-            R.drawable.national_waterfront_museum, R.drawable.rhosilli, R.drawable.three_cliffs_bay, R.drawable.swansea_museum_dylan_thomas_centre,
-            )
-//    ditto but of names (at least the strings are externalised)
-        val myImageNameList = arrayOf(R.string.swanseaCastle, R.string.mumbles, R.string.clyneGarden,
-            R.string.nationalWaterfront, R.string.rhosilli, R.string.threeCliff, R.string.swanseaMuseumDylan)
-
-//    Wrapping up an image and a name in the model class
-        for (i in 0..6) {
-            val imageModel = Thumbnails_poi()
-            imageModel.setThumbnailText(getString(myImageNameList[i]))
-            imageModel.setThumbnailImage(myImageList[i])
-            list.add(imageModel)
+        val mainButton = findViewById<Button>(R.id.buttonMain)
+        mainButton.setOnClickListener{
+            val thumbnailIntent = Intent(this@MainActivity, ThumbnailActivity::class.java)
+            startActivity(thumbnailIntent)
         }
-        //sorting alphabetically
-        //list.sortBy { list -> list.modelName }
-        return list
     }
+
 }
